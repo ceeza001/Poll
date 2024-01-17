5b0745e6d3716e23ca55bb5f9d61c343f18bc37e
ceeza001 <mesalokubor@gmail.com>
2024-01-17 21:29:41 +0000

    update

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -58,12 +58,12 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+        <h1 className="h1-bold">FC Poll</h1>
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
         </h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">
+        <p className="text-gray-500 small-medium md:base-regular mt-2">
           Welcome back! Please enter your details.
         </p>
         <form
