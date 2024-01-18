21906fc53cb7ce00b2cceb8ca5cb3ccec6948eed
ceeza001 <mesalokubor@gmail.com>
2024-01-18 09:15:32 +0000

    update

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,16 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
 import Topbar from "@/components/shared/Topbar";
+import { useUserContext } from "@/context/AuthContext";
 
 const RootLayout = () => {
+  const { user } = useUserContext();
+  
   return (
     <>
-      <div className="w-full md:flex">
-        <Topbar />
-        <section className="flex flex-1 h-full">
-          <Outlet />
-        </section>
-      </div>
+      {user.name == "" ? (
+        <div className="w-full md:flex">
+          <Topbar />
+          
+          <section className="flex flex-1 h-full">
+            <Outlet />
+          </section>
+        </div>
+      ) : (
+        <Navigate to="/sign-in" />
+      )}
     </>
   );
 };
