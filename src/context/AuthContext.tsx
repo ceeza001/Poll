bbb681e4583685dce9e11e33c42f0c822b7a7741
Ceeza <21365128-Ceeza@users.noreply.replit.com>
2024-04-28 03:03:33 +0000

    update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 import { IUser } from "@/types";
 import { getCurrentUser } from "@/lib/appwrite/api";
