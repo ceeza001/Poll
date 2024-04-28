eb988fd90dfc4d03e4585a22eae0dfa73f0dc3c1
Ceeza <21365128-Ceeza@users.noreply.replit.com>
2024-04-28 02:56:00 +0000

    update

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -19,12 +19,6 @@ import { INewUser } from "@/types";
 // AUTH QUERIES
 // ============================================================
 
-export const useCreateUserAccount = () => {
-  return useMutation({
-    mutationFn: (user: INewUser) => createUserAccount(user),
-  });
-};
-
 export const useSignInAccount = () => {
   return useMutation({
     mutationFn: (user: { email: string; password: string }) =>
