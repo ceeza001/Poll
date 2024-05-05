886df04f8d74e5b5f1387532f4c2c7a0150e8096
Ceeza <21365128-Ceeza@users.noreply.replit.com>
2024-05-05 17:54:53 +0000

    update

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,4 +1,3 @@
 export { default as TitleForm } from "./TitleForm";
 export { default as DescriptionForm } from "./DescriptionForm";
-export { default as FileUploadForm } from "./FileUploadForm";
 export { default as CandidatesForm } from "./CandidatesForm";
